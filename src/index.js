7f39a756d279bb55a9831ca96d9e517c7fe5a195
GrafDracula_BlaBlaBla <leo95_11@mail.ru>
2018-09-27 11:59:44 +0300

    quadratic-equation

--- src/index.js
+++ src/index.js
@@ -1,3 +1,37 @@
 module.exports = function solveEquation(equation) {
-  // your implementation
-}
+  	let rootsEquation = [];
+  	let variableEquation,
+  	 	discremenenant,
+  	 	x1,
+  	 	x2;
+
+	// a * x^2 - b * x + c
+	variableEquation = equation.match(/(\b[\d]{2})|(\b[\w])/i);
+	
+	// discremenenant = b^2 - 4 * a * c
+	discremenenant = (variableEquation[3] * variableEquation[3]) - (4 * variableEquation[0] * variableEquation[5]);
+
+	if (discremenenant > 0) {
+		x1 = (-variableEquation[3] + Math.sqrt(discremenenant)) / 2 * variableEquation[0]
+		x2 = (-variableEquation[3] - Math.sqrt(discremenenant)) / 2 * variableEquation[0]
+		if (x1 > x2 ) {
+			rootsEquation.push(x2, x1);
+		} else {
+			rootsEquation.push(x1, x2);
+		}
+	} else if (discremenenant === 0) {
+		return x1 = variableEquation[3] / 2 * variableEquation[0];
+	} else if (discremenenant < 0) {
+		return rootsEquation;
+	} 
+	return rootsEquation;	
+}	
+
+	  
+
+/*-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=*/
+
+
+
+
+/*-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=*/
